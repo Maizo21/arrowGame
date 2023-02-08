8c2542e08dd9b8e8222ae36490d3a5309badc565
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2023-02-07 21:16:12 -0300

    Modificaciones en la creacion bidimensional del mapa en canvas

--- game.js
+++ game.js
@@ -10,13 +10,32 @@ window.addEventListener("resize", setCanvasSize);
 
 function startGame() {
   game.font = elementSize * 0.8 + "px Verdana";
-
   game.fillStyle = "blue";
   game.textAlign = "end";
 
-  for (let i = 0; i <= 10; i++) {
-    game.fillText(emojis["X"], elementSize, elementSize * i);
-  }
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapRowsCols = mapRows.map((row) => row.trim().split(""));
+
+  mapRowsCols.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      const emoji = emojis[col];
+      const posX = elementSize * (colIndex + 1);
+      const posY = elementSize * (rowIndex + 1);
+      game.fillText(emoji, posX, posY);
+      console.log(col);
+    });
+  });
+
+  /*   for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      game.fillText(
+        emojis[mapRowsCols[i - 1][j - 1]],
+        elementSize * j,
+        elementSize * i
+      );
+    }
+  } */
 }
 
 function setCanvasSize() {
